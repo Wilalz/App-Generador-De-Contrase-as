8b5d1fb9696fcf48aa7fb4243e35098ad13f5d4b
WilAlz <wilalzatep@gmail.com>
2024-05-04 00:57:41 -0500

    Crear archivos HTML, CSS y JavaScript para el generador de contraseñas

---
+++ README.md
@@ -0,0 +1,25 @@
+# Generar contraseñas seguras - Generate Strong Passwords
+Una pequeña app para generar contraseñas seguras compuestas de 15 caracteres combinados. Desarrollada con HTML, CSS y JavaScript)
+
+## Ver la presentación!
+[Web -> Generador de contraseñas](https://wilalz.github.io/Landing-page-responsive-Real-estate-Figma-HTML-CSS/)
+
+<!-- imagen -->
+![app](https://github.com/Wilalz/Landing-page-responsive-Real-estate-Figma-HTML-CSS/blob/main/assets/Web%20resultante%20-%20Landing%20page%20Real%20estate.jpg)
+
+## Instalación
+Clona o descarga el repósitorio\
+Abre index.html desde un navegador web
+
+## Tecnologias utilizadas
+HTML | CSS | JavaScript
+
+## Participantes
+Wilson Alzate
+
+## Contacto
+[Wilson Alzate (LinkedIn)](https://www.linkedin.com/in/wilson-alzate-pineda/)
+
+
+
+## .
---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById("btnGenerate");
+
+const pw = document.getElementById("passWord")
+
+const abcarr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+const abcDEFarr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_", "-", "/", "!", "@", "#", "$", "%", "&", "?", ";", ":", "(", ")", "*", "+" ]
+
+const compArr = abcarr.concat(abcDEFarr)
+
+
+btn.addEventListener("click", generadorClavesGoogle)
+
+
+function generadorClavesGoogle () {
+
+    for (let i = 0; i < 100 ; i++){
+        const ramdomly = () => {
+            return compArr[Math.round(Math.random()*77)]
+        }
+        pw.textContent = `${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}${ramdomly()}`
+    }
+}
